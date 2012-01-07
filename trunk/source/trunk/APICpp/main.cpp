3a726cfca7e9c690d178ac3b05aadb12ffab352d
cvicente <cvicente@5a9b6f66-68dc-6f08-c445-8a32abf40b3d>
2012-01-07 00:15:42 +0000

    Small changes and bug corrections.

--- trunk/source/trunk/APICpp/main.cpp
+++ trunk/source/trunk/APICpp/main.cpp
@@ -9,12 +9,36 @@
  * $Revision$
  * */
 
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
 #include "ZB_MonitoringAndControl.h"
 #include "ZB_Node.h"
 #include "API_IO_Sample.h"
 #include "API_AT_RemoteCommandResponse.h"
 
- using namespace std;
+using namespace std;
+
+bool unitTesting()
+{
+
+    // Get the top level suite from the registry
+    CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+
+    // Adds the test to the list of test to run
+    CppUnit::TextUi::TestRunner runner;
+    runner.addTest( suite );
+
+    // Change the default outputter to a compiler error format outputter
+    runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(),
+                                                           std::cerr ) );
+    // Run the tests.
+    bool wasSucessful = runner.run();
+
+    // Return error code 1 if the one of test failed.
+    return wasSucessful ? 0 : 1;
+}
 
 double voltage_divider(vector<int>& imp_elements, int grounded_element, double Vin, double Vout)
 {
@@ -423,8 +447,9 @@ void useMC()
 
  int main()
  {
+     unitTesting();
      //useTXRX();
-     useMC();
+     //useMC();
 
      return 0;
  }
